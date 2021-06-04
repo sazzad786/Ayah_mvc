5364ce1a0db88191cedfda71e4f60a7d44ff1dc1
S Rahman <sazzad10@gmail.com>
2021-06-04 22:57:19 +0100

    initial check-ins

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 const express = require("express"); //download express library from npm
 
 const chalk = require("chalk"); //print message in color code
-
+//console.log(chalk.yellow("Program started"));
+//console.log(chalk.red("Error while starting program"));
+const fs = require("fs");
 const app = express();
+var products = {};
+var isUserLoggedIn = false;
 
-console.log(chalk.yellow("Program started"));
+var PORT = process.env.PORT || 8080;
 
-console.log(chalk.red("Error while starting program"));
+fs.readFile("./data/products.json", function(err, data) {
+  products = JSON.parse(data.toString());
+});
 
 /* starting server might take few sec **/
-app.listen(3000, function() {
-  console.log(chalk.green("Server started listening on port 3000"));
+//3000 local Port / web port:8080 --->> PORT
+app.listen(PORT, function() {
+  console.log(chalk.green("Server started listening on port", PORT));
 });
+
+//middleware or tunnel
+app.use(function(req, res, next) {
+  console.log("*** Request received *** : ", new Date());
+  next(); // pass request to next handler
+  // i want to check whether to next handler
+  // i want to check user is logged in or not
+  // i want to print request received date for every handler
+});
+
+function validateUser(req, res, next) {
+  if (isUserLoggedIn) {
+    next();
+  } else {
+    //res.send('User not logged in !');
+    res.sendFile(__dirname + "/login.html");
+  }
+}
 
 /* http://localhost:3000/ */
 app.get("/", function(req, res) {
@@ -20,12 +45,22 @@ app.get("/", function(req, res) {
 
 /* http://localhost:3000/login */
 app.post("/login", function(req, res) {
-  res.send("<h3>Login Success</h3>");
+  //res.send("<h3>Login Success</h3>");
+  isUserLoggedIn = true;
+  res.sendFile(__dirname + "/home.html");
 });
 
 /* http://localhost:3000/products */
-app.get("/products", function(req, res) {
+/*app.get("/products", function(req, res) {
   return products;
+  */
+app.get("/invoice", validateUser, function(req, res) {
+  res.download(__dirname + "/data/invoice.pdf");
+});
+
+app.get("/invoice", function(req, res) {
+  //res.download(__dirname + "/data/invoice.pdf");
+  res.json(products);
 });
 
 console.log("**** Program ended ****");
